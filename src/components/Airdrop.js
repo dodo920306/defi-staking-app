1a4bcb8b4b723f45b890458e0e51a47a21b24730
dodo920306 <dodo920306@gmail.com>
2023-02-13 00:15:11 +0800

    re-typeset

--- src/components/Airdrop.js
+++ src/components/Airdrop.js
@@ -1,68 +1,68 @@
 import React, { Component } from "react";
 
 class Airdrop extends Component {
-    constructor() {
-        super();
-        this.state = {
-        time: {},
-        seconds: 1200,
-        };
-        this.timer = 0;
-        this.startTimer = this.startTimer.bind(this);
-        this.countDown = this.countDown.bind(this);
-    }
+  constructor() {
+    super();
+    this.state = {
+      time: {},
+      seconds: 1200,
+    };
+    this.timer = 0;
+    this.startTimer = this.startTimer.bind(this);
+    this.countDown = this.countDown.bind(this);
+  }
 
-    startTimer() {
-        if(this.timer === 0 && this.state.seconds > 0) {
-            this.timer = setInterval(this.countDown, 1000);
-        }
+  startTimer() {
+    if (this.timer === 0 && this.state.seconds > 0) {
+      this.timer = setInterval(this.countDown, 1000);
     }
+  }
 
-    countDown() {
-        let seconds = this.state.seconds - 1;
-        this.setState({
-            time: this.secondsToTime(seconds),
-            seconds: seconds
-        })
+  countDown() {
+    let seconds = this.state.seconds - 1;
+    this.setState({
+      time: this.secondsToTime(seconds),
+      seconds: seconds,
+    });
 
-        if(seconds === 0) {
-            clearInterval(this.timer);
-            this.props.issueTokens();
-        }
+    if (seconds === 0) {
+      clearInterval(this.timer);
+      this.props.issueTokens();
     }
+  }
 
-    secondsToTime(secs) {
-        let hours = Math.floor(secs / 3600),
-            minutes = Math.floor((secs % 3600) / 60),
-            seconds = secs % 3600 % 60,
-            obj = {
-                'h': hours,
-                'm': minutes,
-                's': ("0" + seconds.toString()).slice(-2)
-            };
-        return obj;
-    }
+  secondsToTime(secs) {
+    let hours = Math.floor(secs / 3600),
+      minutes = Math.floor((secs % 3600) / 60),
+      seconds = (secs % 3600) % 60,
+      obj = {
+        h: hours,
+        m: minutes,
+        s: ("0" + seconds.toString()).slice(-2),
+      };
+    return obj;
+  }
 
-    componentDidMount() {
-        let timeLeft = this.secondsToTime(this.state.seconds);
-        this.setState({time: timeLeft});
-    }
+  componentDidMount() {
+    let timeLeft = this.secondsToTime(this.state.seconds);
+    this.setState({ time: timeLeft });
+  }
 
-    airdropReleaseTokens() {
-        let stakingBalance = this.props.stakingBalance;
-        if (stakingBalance >= window.web3.utils.toWei('50')){
-            this.startTimer();
-        }
+  airdropReleaseTokens() {
+    let stakingBalance = this.props.stakingBalance;
+    if (stakingBalance >= window.web3.utils.toWei("50")) {
+      this.startTimer();
     }
+  }
 
-    render() {
-        this.airdropReleaseTokens();
-        return (
-            <div style={{color:'black'}}>
-                {this.state.time.m}:{this.state.time.s}
-            </div>
-        );
-    }
+  render() {
+    this.airdropReleaseTokens();
+    return (
+      <div style={{ color: "black" }}>
+        {this.state.time.m}:{this.state.time.s}
+      </div>
+    );
+  }
 }
 
 export default Airdrop;
